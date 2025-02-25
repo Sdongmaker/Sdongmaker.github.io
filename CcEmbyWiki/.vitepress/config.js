dc20b2ceae373e56381f236086f529a58acec5bd
xiaolilalala <13470423+xiaolilalala@user.noreply.gitee.com>
2025-02-25 21:19:51 +0800

    111

--- CcEmbyWiki/.vitepress/config.js
+++ CcEmbyWiki/.vitepress/config.js
@@ -1,37 +1,39 @@
 export default {
-    title: '起点站 Wiki',
-    description: '基于 VitePress 的 起点站 文档站点',
-    themeConfig: {
-      nav: [
-        { text: '首页', link: '/' },
-        {
-          text: '起点站附属项目',
-          items: [
-            {
-              text: '起点机场',
-              link: '/column/Travel/' // 表示docs/column/Travel/index.md
-            },
-            {
-              text: '起点私服',
-              link: '/column/Growing/' // 表示docs/column/Growing/index.md
-            }
-          ]
-        },
-      ],
-      sidebar: {
-        '/guide/': [
-          { text: '   ', link: '' },
-          { text: '   ', link: '' },
-          { text: 'Emby 初体验', link: '/guide/emby-intro' },
-          { text: '如何注册与保持', link: '/guide/registration' },
-          { text: '使用教程', link: '/guide/tutorial' },
-          { text: '公益服使用条款', link: '/guide/terms' },
-          { text: 'Emby 客户端分享', link: '/guide/client-sharing' },
-          { text: '常见问题', link: '/guide/faq' },
-          { text: '隐私与安全', link: '/guide/privacy' },
-          { text: '关于 起点站', link: '/guide/about' }
+  title: '起点站 Wiki',
+  description: '基于 VitePress 的 起点站 文档站点',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '起点站附属项目',
+        items: [
+          {
+            text: '起点机场',
+            link: '/column/Travel/' // 表示docs/column/Travel/index.md
+          },
+          {
+            text: '起点私服',
+            link: '/column/Growing/' // 表示docs/column/Growing/index.md
+          }
         ]
-      }
+      },
+    ],
+    sidebar: {
+      '/guide/': [
+        { text: '   ', link: '' },
+        { text: '   ', link: '' },
+        { text: 'Emby 初体验', link: '/guide/emby-intro' },
+        { text: '如何注册与保持', link: '/guide/registration' },
+        { text: '使用教程', link: '/guide/tutorial' },
+        { text: '公益服使用条款', link: '/guide/terms' },
+        { text: 'Emby 客户端分享', link: '/guide/client-sharing' },
+        { text: '常见问题', link: '/guide/faq' },
+        { text: '隐私与安全', link: '/guide/privacy' },
+        { text: '关于 起点站', link: '/guide/about' }
+      ]
+    },
+    outline: {
+      label: '本页内容' // 修改 "On this page" 为中文
     }
   }
-  
+}
--- CcEmbyWiki/index.md
+++ CcEmbyWiki/index.md
@@ -26,16 +26,18 @@ features:
   - icon: 🎥
     title: 高速流媒体服务
     details: 为您提供无与伦比的高清流媒体体验，支持 Emby 和 TG bot 求片功能，利用自建直连 CDN 提供流畅播放。无论您在哪里，都能享受最佳观看体验。
-    link: /column/views/guide
+    link: /guide/index2
   - icon: 🤹‍♀️
     title: 高效前端技术
     details: 关注最新 Web 前端技术，致力于打造极致流畅的用户体验，为您的每一次点击提供无缝连接。我们的开发追求高效与创新，让您更快享受服务。
-    link: /column/views/guide
+    link: /guide/index2
   - icon: 👩🎨
     title: 超高的性价比
     details: 设计与美学的完美结合，我们专注于界面与交互设计，力求为您提供直观、愉悦的视觉体验，带来每一次的沉浸感。
+    link: /guide/index2
   - icon: 🧩
     title: 斜杠青年精神
     details: 我们不仅专注于流媒体服务，还探索多重职业发展路径。通过跨领域学习与成长，不断完善自我，保持创新与活力，时刻为用户提供前沿技术与优质服务。
+    link: /guide/index2
 
 ---
