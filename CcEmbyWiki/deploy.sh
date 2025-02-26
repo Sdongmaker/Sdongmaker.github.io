0c2b84d136fc388224240be00a42dadeb1bb3a55
唐师傅 <shengdongtang8@gmail.com>
2025-02-27 02:02:04 +0800

    更新文件权限并优化部署脚本以支持 gh-pages 分支推送

--- CcEmbyWiki/deploy.sh
+++ CcEmbyWiki/deploy.sh
@@ -1,23 +1,25 @@
-# filepath: /Users/sdongmaker/VScode/wiki/CcEmbyWiki/deploy.sh
-#!/usr/bin/env sh
+#!/bin/bash
 
-# 当发生错误时中止脚本
-set -e
-
-# 构建
+# 构建项目
 npm run build
 
-# 进入构建文件夹
+# 进入构建输出目录
 cd .vitepress/dist
 
-# 如果你是要部署到自定义域名
-# echo 'www.example.com' > CNAME
-
+# 初始化一个新的 Git 仓库
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你要部署到 https://<USERNAME>.github.io
-git push -f git@github.com:Sdongmaker/sdongmaker.github.io.git main
+# 检查是否存在 gh-pages 分支
+if git ls-remote --exit-code --heads git@github.com:sdongmaker/wiki.git gh-pages; then
+  echo "gh-pages 分支已存在，推送到 gh-pages 分支"
+else
+  echo "gh-pages 分支不存在，创建并推送到 gh-pages 分支"
+fi
 
+# 强制推送到 gh-pages 分支
+git push -f git@github.com:sdongmaker/wiki.git master:gh-pages
+
+# 返回到项目根目录
 cd -
