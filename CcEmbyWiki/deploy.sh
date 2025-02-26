749f9b2c5f65b7a0268982b22cf4d1e52b8e6760
唐师傅 <shengdongtang8@gmail.com>
2025-02-27 02:37:24 +0800

    更新部署脚本以推送到新的 gh-pages 分支，并添加重定向 HTML 文件

--- CcEmbyWiki/deploy.sh
+++ CcEmbyWiki/deploy.sh
@@ -11,15 +11,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 检查是否存在 gh-pages 分支
-if git ls-remote --exit-code --heads git@github.com:sdongmaker/wiki.git gh-pages; then
-  echo "gh-pages 分支已存在，推送到 gh-pages 分支"
-else
-  echo "gh-pages 分支不存在，创建并推送到 gh-pages 分支"
-fi
-
 # 强制推送到 gh-pages 分支
-git push -f git@github.com:sdongmaker/wiki.git master:gh-pages
+git push -f https://github.com/Sdongmaker/Sdongmaker.github.io.git main:gh-pages
 
 # 返回到项目根目录
 cd -
