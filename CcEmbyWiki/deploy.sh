831563b31817827deaf4ed3996c183a1d3625edf
唐师傅 <shengdongtang8@gmail.com>
2025-02-27 03:11:55 +0800

    优化部署脚本，移除不必要的 Git 初始化和推送步骤

--- CcEmbyWiki/deploy.sh
+++ CcEmbyWiki/deploy.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# 构建项目
 npm run build
-
-# 进入构建输出目录
 cd .vitepress/dist
-
-# 初始化一个新的 Git 仓库
 git init
 git add -A
 git commit -m 'deploy'
-
-# 强制推送到 gh-pages 分支
 git push -f https://github.com/Sdongmaker/Sdongmaker.github.io.git main:gh-pages
-
-# 返回到项目根目录
+rm -rf .git
 cd -
